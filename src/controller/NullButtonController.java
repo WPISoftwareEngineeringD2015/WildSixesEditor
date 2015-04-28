602736aba106405a1b08ff5333f72e36001b1094
Alyssa Graham <AlyssaG@outlook.com>
2015-04-28 17:27:12 -0400

    Small amount of work

--- src/controller/NullButtonController.java
+++ src/controller/NullButtonController.java
@@ -1,5 +1,23 @@
 package controller;
 
-public class NullButtonController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import model.Model;
+
+public class NullButtonController implements ActionListener{
+	Model model;
+	
+	public NullButtonController(Model m) {
+		this.model = m;
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
